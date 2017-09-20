dc747d4bcd7ed83ef4d4cf158f6ca8c5cb5a9fea
Veda <lee078bg@yahoo.com>
2017-09-19 17:24:10 -0700

    Added a make file and a Main.c program for Lab 1

---
+++ CMPE 207 Lab 1/Main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/tcp.h>
+#include <netinet/in.h>
+
+int main() {
+	printf("Hello!\n");
+	return 0;
+}
