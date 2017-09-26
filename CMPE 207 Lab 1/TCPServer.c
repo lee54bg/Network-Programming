e0fb5b78f3d1b5b46c3889aaa9c4f3c492e0950d
lee078bg@yahoo.com <lee078bg@yahoo.com>
2017-09-25 20:07:23 -0700

    Got UDPClient to work properly.  Deleted previous code as they were experimental code to check the functionality of the given API.  Will continue forward with the assignment

---
+++ CMPE 207 Lab 1/TCPServer.c
@@ -0,0 +1,12 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+	
+}
--- CMPE 207 Lab 1/UDPClient.c
+++ CMPE 207 Lab 1/UDPClient.c
@@ -11,15 +11,20 @@
 
 int main(int argc, char const *argv[]) {
 	
+	/* Declare variables for sockets, ip address, etc. */
 	// Create a socket for connection
 	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);
+	network_socket = socket(AF_INET, SOCK_DGRAM, 0);
 
 	struct sockaddr_in server_address;
 	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
+	server_address.sin_port = htons(13);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 
+	int cnctn_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+	
+	if(cnctn_status == -1)
+		printf("The connection is invalid.");
 
 	return 0;
 }
