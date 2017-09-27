5e9d39f2609fda50130ce4db8fea63325b446b13
lee078bg@yahoo.com <lee078bg@yahoo.com>
2017-09-26 17:43:14 -0700

    Added small portion of UDP code.  Still need to test functionality of the UDP Client

--- CMPE 207 Lab 1/UDPClient.c
+++ CMPE 207 Lab 1/UDPClient.c
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[]) {
 	network_socket = socket(AF_INET, SOCK_DGRAM, 0);	
 
 	//Will be used for keeping track of the file lines later
-	int count;
+	int count, length;
 	
 	// Declare the parameters for the server socket the user is trying to connect to
 	struct sockaddr_in server_address;
@@ -26,24 +26,17 @@ int main(int argc, char const *argv[]) {
 	server_address.sin_port = htons(13);		// Get the port 
 	server_address.sin_addr.s_addr = INADDR_ANY;	// Use any IP Address
 	
+	length = sizeof(server_address);
+
 	char daytimeResponse[512];
-	int cc = recvfrom(network_socket, daytimeResponse, sizeof(daytimeResponse), 0);
+	int cc = recvfrom(network_socket, $daytimeResponse, sizeof(daytimeResponse), 0, (struct sockaddr *) &network_socket, );
 	
 	printf("The daytime response from the server is: %s\n", daytimeResponse);	
-	while (cc > 0) {
+	while (1) {
 		daytimeResponse[512 + 1] = '\0';
 		fputs(daytimeResponse, stdout);
 	}
-/*
-	while( (count = read(network_socket, daytimeResponse, sizeof(daytimeResponse))) ) {
-		daytimeResponse[count] = 0;
-		if( fputs(daytimeResponse, stdout) == EOF)
-			err_sys("Can't read from file");
-	}
-*/
 	
-	// Close the socket connected to the Daytime Server
-	close(network_socket);
 	
 	return 0;
 }
