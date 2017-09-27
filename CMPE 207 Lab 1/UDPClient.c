27d5accd519bba9cbeb023bb3c8a1e7687a6cb03
lee078bg@yahoo.com <lee078bg@yahoo.com>
2017-09-27 15:13:36 -0700

    Added the hostent data structure to hold the ip address and other information of the daytime server

--- CMPE 207 Lab 1/UDPClient.c
+++ CMPE 207 Lab 1/UDPClient.c
@@ -7,6 +7,7 @@
 #include <netinet/tcp.h>
 #include <string.h>
 #include <unistd.h>
+#include <netdb.h>
 #define PORT 8080
 #define BUFSIZE 1024
 
@@ -17,20 +18,31 @@ int main(int argc, char const *argv[]) {
 	// status of the network_socket
 	int network_socket;
 	network_socket = socket(AF_INET, SOCK_DGRAM, 0);	
+	struct hostent *server;
 
 	//Will be used for keeping track of the file lines later
 	int count, length;
 	
+	char hostname[] = "time-a.nist.gov";
+	
+	// Test to make sure socket is working properly
 	printf("Set the network socket\n");
 
 	// Declare the parameters for the server socket the user is trying to connect to
 	struct sockaddr_in server_address;
 	int addrlen = sizeof(server_address);
-
-	server_address.sin_family = AF_INET;		// IPv4 Address
-	server_address.sin_port = htons(13);		// Get the port 
-	server_address.sin_addr.s_addr = htonl(INADDR_ANY);	// Use any IP Address
 	
+	server = gethostbyname(hostname);
+	
+	server_address.sin_family = AF_INET;		// IPv4 Address
+	bcopy((char *) server->h_addr,
+		(char *) &server_address.sin_addr.s_addr, server->h_length);
+	//memcpy(&server_address.sin_addr.s_addr, server->h_addr, server->h_length);
+		
+	//server_address.sin_port = htons(13);		// Get the port 
+	//server_address.sin_addr.s_addr = "134.63.4.101";	// Use any IP Address
+	
+	// Bind is only for servers
 	//bind(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 	
 	printf("Successfully binded the ip address\n");
@@ -41,12 +53,15 @@ int main(int argc, char const *argv[]) {
 	
 	test = sendto(network_socket, temp, sizeof(temp), 0, (struct sockaddr *) &server_address, addrlen);
 	
-	printf("Sent the dgram\n");
-
-	if(test < 0)
+	if( test < 0 )
 		printf("Send doesn't work");
+
+	test = recvfrom(network_socket, daytimeResponse, sizeof(daytimeResponse), 0, (struct sockaddr *) &server_address, &addrlen);
 	
-	recvfrom(network_socket, daytimeResponse, sizeof(daytimeResponse), 0, (struct sockaddr *) &server_address, &addrlen);
+	if( test < 0)
+		printf("Receive doesn't work");
+	
+	
 	printf("About to print my dgram received\n");
 	printf("Message is: %s\n", daytimeResponse);
 	
