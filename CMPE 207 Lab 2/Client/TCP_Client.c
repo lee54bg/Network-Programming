ac767e060acdb8c6e9c9a5c0d556ede9c0c1b88c
lee078bg@yahoo.com <lee078bg@yahoo.com>
2017-10-19 04:43:38 -0700

    Finished refactoring TCP_Client.  Going ahead and refactoring the other TCP Server types

--- CMPE 207 Lab 2/Client/TCP_Client.c
+++ CMPE 207 Lab 2/Client/TCP_Client.c
@@ -20,25 +20,30 @@ int main(int argc, char **argv) {
 	bytes_rcvd		= 0,	// Used to hold the number of bytes read in a single read call
 	total_bytes_rcvd	= 0;	// Total number of bytes received from the server
 	
-	
 	char buffer[BUFFERSIZE];	// Buffer to be used for reading and writing data
 	char *file_path;		// The file name to be sent to the server
 	char *ip_address;		// The name of the ip address to be used to connect to the server
-	ssize_t read_return;		// Size of the buffer to be used for reading and writing to the file
 
 	unsigned short server_port = 12345;	// Port number to be used for connection
 	struct sockaddr_in hostaddr, remaddr;	// Initialize client and server address data structure
-	struct timeval timeout;			// Set the timeout for the socket so that the socket is not stuck on read
+	struct timeval timeout;			// Set the timeout for the socket so that the socket is not stuck blocking on read
 	timeout.tv_sec = 2;
 	timeout.tv_usec = 0;
 
-	socklen_t remaddrlen = sizeof(remaddr);		// Length of our remote address
+	// Length of our remote address
+	socklen_t remaddrlen = sizeof(remaddr);
 	
+	// Create our socket and return a socket descriptor and assign to server_socket
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout));
 
-	if ( server_socket < 0)
-		printf("Socket doesn't work");
+	// Set timeout for our socket
+	setsockopt(server_socket, SOL_SOCKET, SO_RCVTIMEO, (char *) &timeout, sizeof(timeout));
+	
+	// Error checking for socket creation
+	if ( server_socket < 0) {
+		perror("Error in socket creation : ");
+		exit(EXIT_FAILURE);
+	}	
 
 	memset((char *) &remaddr, 0, sizeof(remaddr));
 	memset((char *) &buffer, 0, sizeof(buffer));
@@ -67,9 +72,11 @@ int main(int argc, char **argv) {
 			remaddr.sin_addr.s_addr	= inet_addr(ip_address);	// IP Address of our server
 			break;	
 	}
-
+	
+	// Connect to the server
 	socket_status = connect(server_socket, (struct sockaddr *) &remaddr, remaddrlen);
-
+	
+	// More error checking for the socket descriptor
 	if(socket_status < 0) {
 		perror("Unable to establish connection");
             	exit(EXIT_FAILURE);
@@ -81,44 +88,53 @@ int main(int argc, char **argv) {
 		exit(EXIT_FAILURE);
 	}
 
+	// Open the file and create the file if it doesn't exist
 	filedes = open(file_path,
 		O_WRONLY | O_CREAT | O_TRUNC,
 		S_IRUSR | S_IWUSR);
 	
+	// if theres an error, then terminate the program, otherwise continue with normal read operation
 	if (filedes == -1) {
 		perror("Error in creating file");
             	exit(EXIT_FAILURE);
-	}
+	} else {
+		while(1) {
+			bytes_rcvd = read(server_socket, buffer, BUFFERSIZE);
+			
+			// Break from the loop if you receive a buffer less than 0		
+			if(bytes_rcvd <= 0)
+				break;
 
-	while(1) {
-		bytes_rcvd = read(server_socket, buffer, BUFFERSIZE);
+			// Add bytes received to total amount of bytes read
+			total_bytes_rcvd += bytes_rcvd;
+
+			if (bytes_rcvd < 0) {
+		        	perror("read");
+			        exit(EXIT_FAILURE);
+			}
+
+			if (write(filedes, buffer, bytes_rcvd) < 0) {
+				perror("Failed to write to file");
+				exit(EXIT_FAILURE);
+			}
 		
-		if(bytes_rcvd <= 0)
-			break;
-
-		total_bytes_rcvd += bytes_rcvd;
-
-		if (bytes_rcvd == -1) {
-                	perror("read");
-	                exit(EXIT_FAILURE);
+			memset(buffer, 0, BUFFERSIZE);
 		}
 
-		if (write(filedes, buffer, bytes_rcvd) == -1) {
-			perror("Failed to write to file");
-			exit(EXIT_FAILURE);
-		}
-		
+		printf("The total number of bytes received: %d\n", total_bytes_rcvd);
+
 		memset(buffer, 0, BUFFERSIZE);
+		// Convert the int total_bytes_rcvd into a character array buffer
+		sprintf(buffer, "%d", total_bytes_rcvd);
+		// Sending the amount of bytes received to the server
+		write(server_socket, buffer, BUFFERSIZE);
+
+		// Close your file descriptors
+		close(filedes);
+		close(server_socket);
 	}
 
-	printf("The total number of bytes received: %d\n", total_bytes_rcvd);
-
-	memset(buffer, 0, BUFFERSIZE);
-	sprintf(buffer, "%d", total_bytes_rcvd);
-	write(server_socket, buffer, BUFFERSIZE);	// Sending the amount of bytes received to the server
-
-	close(filedes);
-	close(server_socket);
+	
 
 	return 0;
 }
